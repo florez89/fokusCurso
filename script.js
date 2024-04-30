823042728b74365b6111260df05ab046a0ab422b
Gustavo Florez <florezg217@gmail.com>
2024-04-30 14:54:06 -0300

    Desarrollo del módulo 4 - aplicando audio y estilos

--- script.js
+++ script.js
@@ -4,22 +4,44 @@ const botonCorto = document.querySelector('.app__card-button--corto');
 const botonLargo = document.querySelector('.app__card-button--largo');
 const banner = document.querySelector('.app__image');
 const titulo = document.querySelector('.app__title');
+const botones = document.querySelectorAll('.app__card-button');
+const inputEnfoqueMusica = document.querySelector('#alternar-musica');
+const musica = new Audio('./sonidos/luna-rise-part-one.mp3');
+
+musica.loop = true
+
+inputEnfoqueMusica.addEventListener('change', ()=> {
+        if(musica.paused){
+            musica.play()
+        }else{
+            musica.pause()
+        }
+
+})
 
 botonEnfoque.addEventListener('click', () => {
     CambiarContexto('enfoque')
+    botonEnfoque.classList.add('active')
 
 });
 
 botonCorto.addEventListener('click', () => {
     CambiarContexto('descanso-corto')
+    botonCorto.classList.add('active')
 });
 
 botonLargo.addEventListener('click', () => {
     CambiarContexto('descanso-largo')
+    botonLargo.classList.add('active')
 });
 
 
 function CambiarContexto(contexto){
+
+    botones.forEach(function(contexto){
+        contexto.classList.remove('active')
+    })
+
     html.setAttribute('data-contexto',contexto)
     banner.setAttribute('src', `./imagenes/${contexto}.png`)
 
