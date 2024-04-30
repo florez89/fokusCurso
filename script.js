0e4ec014012288b0df43af4cc009359a944711cb
Gustavo Florez <florezg217@gmail.com>
2024-04-30 10:50:04 -0300

    pasos realizados en el módulo 1

---
+++ script.js
@@ -0,0 +1,20 @@
+const html = document.querySelector('html');
+const botonEnfoque = document.querySelector('.app__card-button--enfoque');
+const botonCorto = document.querySelector('.app__card-button--corto');
+const botonLargo = document.querySelector('.app__card-button--largo');
+const banner = document.querySelector('.app__image');
+
+botonEnfoque.addEventListener('click', () => {
+    html.setAttribute('data-contexto','enfoque');
+});
+
+botonCorto.addEventListener('click', () => {
+    html.setAttribute('data-contexto','descanso-corto');
+});
+
+botonLargo.addEventListener('click', () => {
+    html.setAttribute('data-contexto','descanso-largo');
+});
+
+    
+
